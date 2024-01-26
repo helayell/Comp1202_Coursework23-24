dee0fbded3b7b664e952c6ae80892fdada5282fe
helayell <71077875+helayell@users.noreply.github.com>
2024-01-26 23:47:26 +0000

    Add files via upload

---
+++ src/facilities/Facility.java
@@ -0,0 +1,16 @@
+package facilities;
+
+public class Facility {
+
+  private String name;
+
+  public Facility(String name) {
+    this.name = name;
+  }
+
+  public Facility() {}
+
+  public String getName() {
+    return name;
+  }
+}
